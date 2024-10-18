28133526bff0a998597279a589eaae844009a45e
Cynthia <77578840+cynthia-wzy@users.noreply.github.com>
2024-10-18 14:09:12 +0800

    add comment and top chunks to output

--- rag_csr.py
+++ rag_csr.py
@@ -54,6 +54,9 @@ def load_reranker(reranker):
 
 def query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, chunk_overlaps, num_chunks, rerank_chunks):
     df = pd.read_csv(pointer_path)
+
+    # ranker = Reranker(reranker, model_type = "cross-encoder")
+
     collection_names = get_collection_names(chunk_sizes, chunk_overlaps)
 
     for model_name in model_names:
@@ -68,9 +71,9 @@ def query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, ch
             results = []
             retry_pointers = []
             for idx, row in df.iterrows():
-                pointer_num = row["pointer_num"]
-                pointer_name = row["pointer_name"]
-                definition = row["description"]
+                pointer_num = row["代碼"]
+                pointer_name = row["重大議題"]
+                definition = row["完整定義 (大型語意模型用_尚未修正)"]
                 query = f'此報告書是否揭露"{pointer_name}"? "{pointer_name}"定義如下：{definition}'
 
                 retrieved_results = db.similarity_search(query = query, k = num_chunks)
@@ -80,7 +83,8 @@ def query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, ch
                 rerank_results = ranker.rank(query = query, docs = docs)
 
                 retrieved_res_content = [rerank_result.text for rerank_result in rerank_results]
-                top_chunks = "\n".join([f"{i+1}. {chunk}" for i, chunk in enumerate(retrieved_res_content[:rerank_chunks])])
+                top_chunks = "\n\n".join([f"chunk_{i+1}: {chunk}" for i, chunk in enumerate(retrieved_res_content[:rerank_chunks])])
+                # print(retrieved_res_content, "\n")
                 # print(top_chunks)
 
                 user_template = create_user_template(pointer_name, definition, top_chunks)
@@ -100,7 +104,7 @@ def query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, ch
                 if "答案：0" not in res.content and "答案：1" not in res.content:
                     retry_pointers.append([pointer_num, pointer_name, definition, top_chunks])
                 else:
-                    results.append([pointer_num, res.content])
+                    results.append([pointer_num, res.content, top_chunks])
                     print(res.content) # type: str
 
             # call gpt api again for answers with correct format until no pointer has to be retried
@@ -115,12 +119,12 @@ def query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, ch
 
                     if "答案：0" in res.content or "答案：1" in res.content:
                         del retry_pointers[i]
-                        results.append([pointer_num, res.content])
+                        results.append([pointer_num, res.content, top_chunks])
                         print(res.content) # type: str
 
             print(results)
 
-    return results 
+    return results # also return topk
 
 def get_report_info(csr_path):
     pattern = r"(\d{4})_(\d+)\.pdf"
@@ -131,30 +135,46 @@ def get_report_info(csr_path):
 
     return report_year, company_num
 
-def get_output(csr_path, answer_path, results, output_df):
+def get_output(csr_path, answer_path, comment_path, results, output_df):
     df_answer = pd.read_csv(answer_path)
     df_answer = df_answer.dropna(subset = ["A0002"]) # drop NA
+    df_answer["A0002"] = df_answer["A0002"].str.replace("_", "")
     df_answer["A0002"] = df_answer["A0002"].astype(int).astype(str) # float -> int -> str (for loc)
     df_answer["A0002"] = df_answer["A0002"].str.zfill(8) # company_num needs to be 8 digits
     df_answer.set_index("A0002", inplace = True)
+
+    df_comment = pd.read_csv(comment_path)
+    df_comment.set_index("tax_id", inplace = True)
     
+    # check the answers
     correct = 0
     output = {}
     company_num = get_report_info(csr_path)[1]
     output["company_num"] = company_num
 
     for result in results:
+        print("my result: ", result)
         pointer_num = result[0]
         result_content = result[1]
+        top_chunks = result[2] 
+
+        answer = df_answer.loc[company_num, pointer_num] 
+        comment = df_comment.loc[company_num, pointer_num] # 之後調成可處理多個人評同本報告書
 
         if "答案：0" in result_content:
             output[f"{pointer_num}_output"] = 0
-            output[f"{pointer_num}_answer"] = df_answer.loc[company_num, pointer_num] 
+            output[f"{pointer_num}_answer"] = answer
             output[f"{pointer_num}_reason"] = ""
+            output[f"{pointer_num}_comment"] = comment
+            output[f"{pointer_num}_top_chunks"] = top_chunks
         else: # "答案：1" 
             output[f"{pointer_num}_output"] = 1
-            output[f"{pointer_num}_answer"] = df_answer.loc[company_num, pointer_num]
+            output[f"{pointer_num}_answer"] = answer
             output[f"{pointer_num}_reason"] = result_content.split('理由：')[1]
+            output[f"{pointer_num}_comment"] = comment
+            output[f"{pointer_num}_top_chunks"] = top_chunks
+
+        print(output)
 
         correct += int(output[f"{pointer_num}_output"] == output[f"{pointer_num}_answer"]) # see if the output is correct
     
@@ -167,16 +187,17 @@ def get_output(csr_path, answer_path, results, output_df):
     return output_df
 
 def main():
-    csr_paths = ["csr_reports/2022_03374805.pdf", "csr_reports/2022_04200199.pdf", "csr_reports/2022_05155853.pdf"]
-    pointer_path = "pointers.csv"
-    answer_path = "test_risk_2022.csv"
+    csr_paths = ["csr_reports/2023_03279507.pdf", "csr_reports/2023_03458902.pdf", "csr_reports/2023_04541302.pdf"]
+    pointer_path = "pointers_definition.csv"
+    answer_path = "2023_answer.csv"
+    comment_path = "comments_2023.csv"
     # chunk_sizes = [200, 300, 400, 500]
     # chunk_overlaps = [50, 100]
     # model_names = ["aspire/acge_text_embedding", "intfloat/multilingual-e5-base", "BAAI/bge-large-zh-v1.5"]
     
-    model_names = ["BAAI/bge-large-zh-v1.5"]
+    model_names = ["moka-ai/m3e-base"] # "moka-ai/m3e-base", "BAAI/bge-large-zh-v1.5"
     reranker = "BAAI/bge-reranker-base"
-    chunk_sizes = [200] # 調大一點
+    chunk_sizes = [800] # 調大一點 200 -> 300 -> 500 -> 800
     chunk_overlaps = [50]
     num_chunks = 10
     rerank_chunks = 3
@@ -187,15 +208,16 @@ def main():
     for csr_path in csr_paths:
         start_time = time.time()
         results = query_retrieval(pointer_path, csr_path, model_names, ranker, chunk_sizes, chunk_overlaps, num_chunks, rerank_chunks)
-        output_df = get_output(csr_path, answer_path, results, output_df)
+        # print(results)
+        output_df = get_output(csr_path, answer_path, comment_path, results, output_df)
         end_time = time.time()
         total_time = end_time - start_time
 
         output_df.loc[output_df["company_num"] == get_report_info(csr_path)[1], "Time spent (sec)"] = f"{total_time:.2f}"
-        print(f"Time spent on {csr_path.replace("csr_reports/", "")}: {total_time:.2f} seconds")
+        print(f'Time spent on {csr_path.replace("csr_reports/", "")}: {total_time:.2f} seconds')
 
     report_year = get_report_info(csr_paths[0])[0] 
-    output_df.to_csv(f"{report_year}_output.csv", index = False, encoding = "utf-8") 
+    output_df.to_csv(f"{report_year}_output_m3e_800_1016.csv", index = False, encoding = "utf-8", lineterminator = "\n") 
     print(f"The combined output for {report_year} has been created.")
 
 if __name__ == "__main__":
